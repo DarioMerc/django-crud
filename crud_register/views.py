069398e3b4ad831baa4256e037eb9efeb011f35c
Dario Mercuri <dario.mercuri31@gmail.com>
2021-12-17 01:03:24 -0500

    initial commit. setup basic routing of pages

---
+++ crud_register/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def employee_list(request):
+    return render(request,"crud_register/crud_list.html")
+def employee_form(request):
+    return render(request,"crud_register/crud_form.html")
+def employee_delete(request):
+    return
